8b057d0777623fbe434b411655a840862f9b0352
Alekksia <alexia.litschgy@polytechnique.edu>
2021-01-28 19:46:10 +0100

    jeudi

--- Rikudo/src/Backtracking.java
+++ Rikudo/src/Backtracking.java
@@ -41,7 +41,6 @@ public class Backtracking {
 		
 		while (k>0 && k<n) {
 			
-			
 		
 		// on cherche le premier voisin
 		int j = 0;
@@ -50,25 +49,29 @@ public class Backtracking {
 		}
 		
 		if (j==n) {
+			
 			// il n'y a plus de voisin à j, on fait demi tour
 			k -=1;
 			if (k==0) {
 				break;    // cas où source n'a plus de voisin
 			}
-			s = path[k];
-			// on remet les arcs qui vont vers s
+			s = path[k-1];
+			// on remet les arcs qui vont vers path[k]
 			for (int i =0; i<n; i+=1) {
 				ArrayList<Boolean> ligne = graph2.get(i);
-				ligne.set(s, graph.get(i).get(s));
+				ligne.set(path[k], graph.get(i).get(path[k]));
 				graph2.set(i, ligne);
 			}
-			// on supprime l'arc qu'on vient de remonter
-			ArrayList<Boolean> ligne = graph.get(path[k]);
-			ligne.set(path[k+1], false);
-			graph.set(path[k], ligne);
-			ArrayList<Boolean> ligne2 = graph2.get(path[k]);
-			ligne2.set(path[k+1], false);
-			graph2.set(path[k], ligne2); 
+			// on supprime provisoirement l'arc qu'on vient de remonter
+			ArrayList<Boolean> ligne2 = graph2.get(path[k-1]);
+			ligne2.set(path[k], false);
+			graph2.set(path[k-1], ligne2); 
+			
+			
+			// on remet les arcs qui partent de path[k]
+			for (int i=0; i<n; i+=1) {
+				graph2.get(path[k]).set(i, graph.get(path[k]).get(i));
+			}
 			
 		}
 		
@@ -105,6 +108,7 @@ public class Backtracking {
 	
 	public static int countBacktracking(ArrayList<ArrayList<Boolean>> graph, int source, int t) {
 		
+		
 		int s = source;
 		
 		int compteur = 0;
@@ -122,24 +126,22 @@ public class Backtracking {
 					graph.set(i, ligne);
 					ArrayList<Boolean> ligne2 = graph.get(t);
 					ligne2.set(i, false);
-					graph.set(t, ligne);
+					graph.set(t, ligne2);
 				}
 		
 		
-		// on crée deux copies du graphe : une pour éviter de passer deux fois par le même sommet, l'autre pour s'assurer que si on a réussi un chemin on ne va pas repasser par le même
+		// on crée une copie du graphe
 		
 		ArrayList<ArrayList<Boolean>> graph2 = new ArrayList<ArrayList<Boolean>>();
-		ArrayList<ArrayList<Boolean>> graph3 = new ArrayList<ArrayList<Boolean>>();
+		
 		
 		for (int i = 0; i<n; i+=1) {
 			
 			ArrayList<Boolean> ligne = graph.get(i);
-			ArrayList<Boolean> ligne2 = new ArrayList<Boolean>();
-			ArrayList<Boolean> ligne3 = new ArrayList<Boolean>();
+			ArrayList<Boolean> ligne2 = new ArrayList<Boolean>();			
 			ligne2.addAll(ligne);
-			ligne3.addAll(ligne);
+			
 			graph2.add(i, ligne2);
-			graph3.add(i, ligne3);
 		}
 		
 		
@@ -150,49 +152,51 @@ public class Backtracking {
 		
 		// pour savoir si il n'y a plus de chemin hamiltonien possible : on est revenus à s et il n'a pas de voisin donc k=0
 		// quand on trouve un chemin : on incrémente
+	
 		
 		while (k>0 ) {
-			
+		
 		if (s==t && k==n) {
 			compteur +=1;
 		}
 	
-			
 		
 		// on cherche le premier voisin
 		int j = 0;
-		while (j<n && ! graph3.get(s).get(j)) {
+		while (j<n && ! graph2.get(s).get(j)) {
 			j+=1;
+			
 		}
 		
+		
+		
 		if (j==n) {
+
 			// il n'y a plus de voisin à j, on fait demi tour
 			k -=1;
 			if (k==0) {
 				break;    // cas où source n'a plus de voisin
 			}
-			s = path[k];
-			// on remet les arcs qui vont vers s
+			s = path[k-1];
+			
+			// on remet les arcs qui vont vers path[k]
 			for (int i =0; i<n; i+=1) {
 				ArrayList<Boolean> ligne = graph2.get(i);
-				ligne.set(s, graph.get(i).get(s));
+				ligne.set(path[k], graph.get(i).get(path[k]));
 				graph2.set(i, ligne);
-				ArrayList<Boolean> ligne3 = graph3.get(i);
-				ligne3.set(s, graph.get(i).get(s));
-				graph3.set(i, ligne3);
 			}
 			// on interdit provisoirement l'arc qu'on vient de remonter
-			ArrayList<Boolean> ligne = graph3.get(path[k]);
-			ligne.set(path[k+1], false);
-			graph3.set(path[k], ligne);
+			ArrayList<Boolean> ligne = graph2.get(s);
+			ligne.set(path[k], false);
+			graph2.set(s, ligne);
 			
 			// on réautorise les arcs qui en partent
 			
-			ArrayList<Boolean> ligne3 = graph3.get(path[k+1]);
+			ArrayList<Boolean> ligne3 = graph2.get(path[k]);
 			for (int i=0; i<n; i+=1) {
-				ligne3.set(i, graph2.get(path[k+1]).get(i));
+				ligne3.set(i, graph.get(path[k]).get(i));
 			}
-			graph3.set(path[k+1], ligne3); 
+			graph2.set(path[k], ligne3); 
 			
 			
 		}
@@ -206,9 +210,6 @@ public class Backtracking {
 				ArrayList<Boolean> ligne = graph2.get(i);
 				ligne.set(j, false);
 				graph2.set(i, ligne);
-				ArrayList<Boolean> ligne3 = graph3.get(i);
-				ligne3.set(s, graph.get(i).get(s));
-				graph3.set(i, ligne);
 			}
 			s = j;	
 		}
@@ -218,13 +219,14 @@ public class Backtracking {
 		
 		
 	}
-		
+		System.out.println("compteur=" + compteur);
 		return compteur;
 		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 			
 		ArrayList<ArrayList<Boolean>> graph = new ArrayList<ArrayList<Boolean>>();
 		
@@ -246,7 +248,7 @@ public class Backtracking {
 		ligne3.add(false);
 		ligne3.add(true);
 		ligne3.add(false);
-		ligne3.add(true);
+		ligne3.add(false);
 		graph.add(ligne3);
 		
 		ArrayList<Boolean> ligne4 = new ArrayList<Boolean>();
@@ -256,8 +258,7 @@ public class Backtracking {
 		ligne4.add(false);
 		graph.add(ligne4);
 		
-		
-		System.out.println(countBacktracking(graph,0,2));
+		countBacktracking(graph,0,2);
 		
 	}
 
